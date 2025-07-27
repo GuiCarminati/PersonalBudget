604e9e8db339024e20083113951fc1eff5935dc4
Gui Carminati <guicarminati@outlook.com>
2025-07-27 18:24:04 +0100

    Create front end skeleton

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ module.exports = app;
 
 const PORT = process.env.PORT || 4001;
 
+app.get((req,res,next)=>{
+    res.status(200).send('Hello, World!')
+});
+
+app.use(express.static('public'));
+
 app.listen(PORT,()=>{
     console.log("Server is listening on port "+PORT);
 });
