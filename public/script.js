2a0a96e5051206de8fd462327792cf63f19bf54b
Gui Carminati <guicarminati@outlook.com>
2025-08-04 00:22:55 +0100

    Create logic to process values obtained from the server and Refactor populate row functions

--- public/script.js
+++ public/script.js
@@ -2,45 +2,79 @@ const totalBalanceTable = document.getElementById('balance-table');
 const totalBalanceRowContainer = document.getElementById('total-balance-row-container');
 const envelopeTableRowsContainer = document.getElementById('envelopes-table-rows');
 
-totalBalanceRowContainer.innerHTML = '';
-envelopeTableRowsContainer.innerHTML = '';
+// totalBalanceRowContainer.innerHTML = '';
+// envelopeTableRowsContainer.innerHTML = '';
 
-// testing values
-const totalBalance = [2200, 1874, 326]; // testing values
-const envelopes = [                     // testing values
-    ['Groceries',500,289,211,1],
-    ['Leisure',200,85,115,2],
-    ['Rent',1500,1500,0,3]
-]; 
-populateRow(totalBalance, totalBalanceRowContainer);
+// // testing values
+// const totalBalance = {                   // testing values
+//     budget: 2200,
+//     used: 1874,
+//     balance: 326
+// }
+// const envelopes = [                     // testing values
+//     {   
+//         name:'Groceries',
+//         budget: 500,
+//         used: 289,
+//         balance: 211,
+//         id: 1
+//     },
+//     {   
+//         name:'Leisure',
+//         budget: 200,
+//         used: 85,
+//         balance: 115,
+//         id: 2
+//     },
+//     {   
+//         name:'Rent',
+//         budget: 1500,
+//         used: 1500,
+//         balance: 0,
+//         id: 3
+//     }
+// ]; 
 
-populateTable(envelopes,envelopeTableRowsContainer);
+// populateRow(totalBalance, totalBalanceRowContainer);
+// populateTable(envelopes, envelopeTableRowsContainer);
 
-function populateRow(arrayOfValues,rowElement,withButton=false){
-    arrayOfValues.forEach((value, index) => {
-        const newCell = document.createElement('td');
-        if(typeof value === 'number'){
-            if(withButton && index===arrayOfValues.length-1){
-                createUpdateEnvelopeButton(value,rowElement);
-            } else {
-                newCell.className = 'currency'; 
-                value = value.toLocaleString(undefined,{ minimumFractionDigits:2 });
-                newCell.innerHTML = value;
-            }
-        } else {
-            newCell.innerHTML = value;
-        }
-        rowElement.appendChild(newCell);
-    });
+function createCell(value,isCurrency=false,className=null,id=null){
+    const newCell = document.createElement('td');
+    if(className) newCell.className = className;
+    if(id) newCell.id = id;
+    if(isCurrency){
+        value = value.toLocaleString(undefined,{ minimumFractionDigits:2 });
+    } 
+    newCell.innerHTML = value;
+    return newCell;
 }
 
-function createUpdateEnvelopeButton(id,rowElement){
+function populateRow(envObj,rowElement){
+    const newRow = {};
+    const isCurrency=true;
+    if(envObj.name){ 
+        // if the obj has a 'name' attribute, it's not the 'Total balance'
+        newRow.newName = createCell(envObj.name);
+    }
+    newRow.newBudget = createCell(envObj.budget,isCurrency,'currency');
+    newRow.newUsed = createCell(envObj.used,isCurrency,'currency');
+    newRow.newBalance = createCell(envObj.balance,isCurrency,'currency');
+    if(envObj.id) {
+        // if the obj has an 'id' attribute, it's not the 'Total balance' and should have a button
+        newRow.newButton = createUpdateEnvelopeButton(envObj.id);
+    }
+    for(const key in newRow){
+        rowElement.appendChild(newRow[key]);
+    }
+}
+
+function createUpdateEnvelopeButton(id){
     const newButton = document.createElement('button');
     newButton.className = 'envelope-button';
     newButton.id = `env-button${id+1}`;
     newButton.innerHTML = 'Update';
     newButton.addEventListener('click',()=>updateEnvelopeButton(id));
-    rowElement.appendChild(newButton);
+    return newButton;
 }
 
 function populateTable(arrayOfValues,tbodyElement){
@@ -52,7 +86,6 @@ function populateTable(arrayOfValues,tbodyElement){
 }
 
 // load envelope values from server
-
 function getAllEnvelopes(){
     fetch('/api/envelopes/')
     .then(response => {
@@ -67,10 +100,42 @@ function getAllEnvelopes(){
     });
 }
 
-function renderAllBalances(){
-    // to do
+function renderAllBalances(envelopesArray){
+    totalBalanceRowContainer.innerHTML = '';
+    envelopeTableRowsContainer.innerHTML = '';
+    console.log(envelopesArray);
+    const sumOfEnvelopes = {
+        budget: 0,
+        used: 0,
+        balance: 0
+    };
+    const allEnvelopes = envelopesArray.map((env) => {
+        const used = env.budget - env.currentBalance;
+        sumOfEnvelopes.budget += env.budget;
+        sumOfEnvelopes.used += used;
+        sumOfEnvelopes.balance += env.currentBalance;
+        return {
+            name: env.name, 
+            budget: env.budget, 
+            used: used, 
+            balance: env.currentBalance, 
+            id: env.id
+        }
+    });
+    // populates My Total Balance values
+    populateRow(sumOfEnvelopes, totalBalanceRowContainer);
+    // populates My Envelopes table
+    populateTable(allEnvelopes, envelopeTableRowsContainer);
 }
 
 function updateEnvelopeButton(id){
+    console.log('update button clicked');
     // to do
 }
+
+function addNewEnvelopeButton(){
+    console.log('add new button clicked');
+    // to do
+}
+
+getAllEnvelopes();
