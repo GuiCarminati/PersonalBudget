2703bf2f201546e85e98430fce980e36dab3e961
Gui Carminati <guicarminati@outlook.com>
2025-08-07 00:06:45 +0100

    Implement transaction addition and history display features in frontend

--- public/addTransaction.js
+++ public/addTransaction.js
@@ -1,36 +1,116 @@
 import { showError } from "./utils.js";
 
-const newEnvelopeFormElement = document.getElementById('new-envelope-form');
-
-newEnvelopeFormElement.addEventListener('submit',handleFormSubmission);
+const addTransactionFormElement = document.getElementById('add-transaction-form');
+const sourceDropdownElement = document.getElementById('transaction-source-id');
+const destinationDropdownElement = document.getElementById('transaction-destination-id');
+const transactionAmountElement = document.getElementById('transaction-amount');
+    
+enableFormSubmission(false);
+loadFormDropdownOptions();
 
 function handleFormSubmission(e){
     e.preventDefault(); // prevents auto page reload when submitting
     console.log('handling form submission');
     
-    const envelopeName = document.getElementById('new-envelope-name').value;
-    const envelopeBudget = document.getElementById('new-envelope-budget').value;
+    const amount = Number(transactionAmountElement.value);
+    let sourceID = sourceDropdownElement.value;
+    let destID = destinationDropdownElement.value;
+
+    sourceID = sourceID==='#' ? null : Number(sourceID);
+    destID = destID==='#' ? null : Number(destID);
     
-    if(!envelopeName) return showError('Invalid Envelope Name!',true);
-
-    if(!envelopeBudget || envelopeBudget <= 0) {
-        return showError('Invalid Envelope Budget!',true);
+    if(validateResponse(amount,sourceID,destID)){
+        const newTransaction = { amount, sourceID, destID };
+        
+        fetch('/api/envelopes/transactions/add', {
+            method:'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(newTransaction)
+        })
+        .then(res => {
+            if(res.ok){
+                return res.json();
+            } else {
+                throw new Error(`${res.status}. Check if the envelope has enough balance.`);
+            }
+        })
+        .then(res => window.alert(`Transaction added: value: ${res.amount}, source: ${res.sourceID}, destination: ${res.destID}`))
+        .catch(error => showError(error,true));
     }
-    const newEnvelope = {
-        name: envelopeName,
-        budget: Number(envelopeBudget),
-        balance: Number(envelopeBudget)
-    };
+}
 
-    fetch('/api/envelopes', {
-        method:'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(newEnvelope)
+function validateResponse(transactionAmount,sourceID,destinationID){
+    const check = (value) => (!value || (typeof value)!='number' || value <= 0);
+    
+    if(check(transactionAmount)){
+        return showError('Invalid amount!',true);
+    }
+
+    // null is accepted for the source and destination IDs
+    // if not null, then check if the values are correct
+    // if one of them is null, the if is skiped
+    if(sourceID!==null && check(sourceID)){
+        return showError('Invalid source!',true);
+    }
+    if(destinationID!==null && check(destinationID)){
+        return showError('Invalid destination!',true);
+    }
+    // but if both of them are null, don't continue
+    if(destinationID===null && sourceID===null){
+        return showError('At least one of the destination or source must be an envelope!',true);
+    }
+    // also halt if both IDs are the same
+    if(destinationID===sourceID){
+        return showError('Source and Destination must be different!',true);
+    }
+    return true;
+}
+
+function loadFormDropdownOptions(){
+    fetch('/api/envelopes/')
+    .then(res => {
+        if(res.ok){
+            return res.json();
+        } else {
+            showError(res);
+        }
     })
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(error => showError(error,true));
+    .then(res => {
+        populateFormDropdownOptions(res);
+        return res;
+    })
+    .then(()=>{
+        enableFormSubmission(true);
+    })
+}
+
+function populateFormDropdownOptions(envelopesList){
+    // add "External Balance" options to list of possible source/destination
+    const extBalance = {id: "#", name: "External Balance"};
+    // add in the begining of the array to appear first in the dropdown options
+    envelopesList.unshift(extBalance); 
+    // add each envelope in the dropdown lists of sources and destinations
+    envelopesList.forEach(env => {
+        const option = document.createElement('option');
+        option.value = env.id;
+        option.innerHTML = `[${env.id}] ${env.name}`
+        const copy = option.cloneNode(true);
+        sourceDropdownElement.appendChild(option);
+        destinationDropdownElement.appendChild(copy);
+    });
+}
+
+// disable form submission before values are loaded in, enables it once it's ready
+function enableFormSubmission(boolean){
+    if(boolean){
+        addTransactionFormElement.addEventListener('submit',handleFormSubmission);
+    } 
+    addTransactionFormElement.disabled = !boolean;
+    const formElements = addTransactionFormElement.childNodes;
+    formElements.forEach(element => {
+        element.disabled = !boolean;
+    });
 }
--- public/transactionsHistory.js
+++ public/transactionsHistory.js
@@ -1,6 +1,70 @@
-import { addNewTransactionButton } from "./utils.js";
+import { addNewTransactionButton, showError, createCell } from "./utils.js";
 
 // Add Transaction button
 const addTransactionBtnElement = document.getElementById('add-transaction-button');
+const transactionsListElement = document.getElementById('transaction-table-list');
+
 addTransactionBtnElement.addEventListener('click',addNewTransactionButton);
+
+let envelopes = [];
+
+loadEnvelopesList();
+
+function populateTransactionsTable(transactionsList){
+    const getEnvelopeById = (id) => envelopes.find(el => el.id === id);
+    transactionsListElement.innerHTML = '';
+    transactionsList.forEach(element => {
+        const newRow = document.createElement('tr');
+        const amount = element.amount;
+        const timestamp = element.timestamp;
+        const sourceID = element.sourceID;
+        const destID = element.destinationID;
+        const sourceName = sourceID ? getEnvelopeById(sourceID).name : 'External Balance';
+        const destName = destID ? getEnvelopeById(destID).name : 'External Balance';        
+        
+        const value = {};
+        value.timestamp = createCell(timestamp);
+        value.source = createCell(`[${sourceID || '#'}] ${sourceName}`);
+        value.dest = createCell(`[${destID || '#'}] ${destName}`);
+        value.amount = createCell(amount,true,'currency');
+
+        for(const key in value){
+            newRow.appendChild(value[key])
+        }
+        transactionsListElement.appendChild(newRow);
+    });
+
+}
+
+function loadTransactionsList(){
+    fetch('/api/envelopes/transactions')
+    .then(res => {
+        if(res.ok){
+            return res.json();
+        } else {
+            showError('Internal Server error');
+            showError(res);
+        }
+    })
+    .then(res => {
+        populateTransactionsTable(res);
+    })
+    .catch(err=>showError(err.message));
+}
+
+function loadEnvelopesList(){
+    fetch('/api/envelopes/')
+    .then(res => {
+        if(res.ok){
+            return res.json();
+        } else {
+            showError(res);
+        }
+    })
+    .then(res => {
+        envelopes = res;
+        loadTransactionsList();
+    })
+}
+
 
